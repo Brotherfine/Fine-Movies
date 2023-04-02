0c57b74868100843e7fd37393dfb192d0179730e
Brotherfine <nxumalo95@gmail.com>
2023-04-02 19:12:17 +0200

    Added pages

---
+++ src/pages/moreInfo.tsx
@@ -0,0 +1,5 @@
+function moreInfo() {
+  return <h1>More Info Tab</h1>;
+}
+
+export default moreInfo;
---
+++ src/pages/trailer.tsx
@@ -0,0 +1,5 @@
+function Trailer() {
+  return <h1>Traler Tab</h1>;
+}
+
+export default Trailer;
