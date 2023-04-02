25ebcbca1dd0eb76d453258b2e8f852d8882394a
Brotherfine <nxumalo95@gmail.com>
2023-04-02 21:42:35 +0200

    created routes for the page

--- src/App.tsx
+++ src/App.tsx
@@ -1,2 +1,46 @@
+import { Routes, Route, Link } from "react-router-dom";
+import Signup from "./pages/signup";
+import Register from "./pages/register";
+import Home from "./pages/home";
+import Favourite from "./pages/favourite";
+import MoreInfo from "./pages/moreInfo";
+import Trailer from "./pages/trailer";
+import NotFound from "./pages/notFound";
 
+function App() {
+  return (
+    <>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/register">Register</Link>
+          </li>
+          <li>
+            <Link to="/home">Home</Link>
+          </li>
+          <li>
+            <Link to="/favourite">Favourite</Link>
+          </li>
+          <li>
+            <Link to="/moreInfo">moreInfo</Link>
+          </li>
+          <li>
+            <Link to="/trailer">Trailer</Link>
+          </li>
+        </ul>
+      </nav>
 
+      <Routes>
+        <Route index element={<Signup />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/favourite" element={<Favourite />} />
+        <Route path="/moreInfo" element={<MoreInfo />} />
+        <Route path="/trailer" element={<Trailer />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
--- src/pages/moreInfo.tsx
+++ src/pages/moreInfo.tsx
@@ -1,5 +1,5 @@
-function moreInfo() {
+function MoreInfo() {
   return <h1>More Info Tab</h1>;
 }
 
-export default moreInfo;
+export default MoreInfo;
