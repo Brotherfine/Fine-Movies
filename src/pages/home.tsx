15d8153670d1deb919f21c8efd83d18724e37069
Brotherfine <nxumalo95@gmail.com>
2023-04-04 21:23:23 +0200

    Initialed the home landing page

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,6 +1,17 @@
-import { Button } from "@chakra-ui/react";
+import {
+  Button,
+  Card,
+  CardBody,
+  CardFooter,
+  CardHeader,
+  Divider,
+  HStack,
+  SimpleGrid,
+  Text,
+} from "@chakra-ui/react";
 import { useNavigate } from "react-router-dom";
 import { useQuery, gql } from "@apollo/client";
+import { InfoIcon, ViewIcon } from "@chakra-ui/icons";
 
 interface Props {
   onBack: (page: string) => void;
@@ -22,11 +33,38 @@ function Home({ onBack }: Props) {
   let navigate = useNavigate();
 
   const { error, loading, data } = useQuery(GET_MOVIES);
-  console.log({ error, loading, data });
+  console.log(data);
 
+  if (loading) return <div> spinner...</div>;
+
+  if (error) return <div>something bad went wrong </div>;
   return (
     <>
-      <h1>Home</h1>
+      <SimpleGrid p="15" spacing={10} minChildWidth="200px">
+        {data.movies.map((movie: any) => {
+          return (
+            <Card borderTop="8px" borderColor="white" key={movie.id}>
+              <CardHeader>{movie.title}</CardHeader>
+              <CardBody>
+                <img src={movie.display_link} alt={movie.title} />
+              </CardBody>
+
+              <Divider />
+              <CardFooter>
+                <HStack>
+                  <Button size="sm" leftIcon={<InfoIcon />} bg="green.400">
+                    Info
+                  </Button>
+                  <Button size="sm" leftIcon={<ViewIcon />} bg="red.700">
+                    Watch Trailer
+                  </Button>
+                </HStack>
+              </CardFooter>
+            </Card>
+          );
+        })}
+      </SimpleGrid>
+
       <Button
         onClick={() => {
           onBack("home");
